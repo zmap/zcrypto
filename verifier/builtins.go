08bab456f52591e2afdf408e252f43e5e759e7c8
David Adrian <davidcadrian@gmail.com>
2017-04-11 19:13:48 -0400

    Add initial zcrypto/verifier
    Verifier is a higher-level module for doing in-depth certificate
    verification, with the goal of being able to impersonate a browser or
    specific root store's validation procedure. It uses the chaining code in
    x509 to generate chains, and then apply's customizable verification on
    top.
    
    This includes a few "built-in" verifiers meant to represent the Firefox,
    Microsoft, Apple, and Java trust stores, respectively.
    
    The package is not yet complete, but should have roughly equivalent
    functionality to x509.ValidateWithStupidDetail, which is now deprecated.

---
+++ verifier/builtins.go
@@ -0,0 +1,60 @@
+/*
+ * ZCrypto Copyright 2017 Regents of the University of Michigan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package verifier
+
+import "github.com/zmap/zcrypto/x509"
+
+// Built-in verifiers representing common root stores.
+var (
+	// NSS is a Verifier mimicking the validation used in Firefox.
+	NSS Verifier
+
+	// Microsoft is a Verifier mimicing the validation in Windows 10 SChannel.
+	Microsoft Verifier
+
+	// Apple is a Verifier mimicing the validation in OS X Sierra SecureTransport.
+	Apple Verifier
+
+	// Java is a Verifier mimicing the validation in Java 8 javax.net.ssl.
+	Java Verifier
+)
+
+// InitializeNSS sets up the built-in NSS Verifier.
+func InitializeNSS(roots, intermediates *x509.CertPool) {
+	NSS.Roots = roots
+	NSS.Intermediates = intermediates
+	NSS.VerifyProcedure = &VerifyProcedureNSS{}
+}
+
+// InitializeMicrosoft sets up the built-in Microsoft Verifier.
+func InitializeMicrosoft(roots, intermediates *x509.CertPool) {
+	Microsoft.Roots = roots
+	Microsoft.Intermediates = intermediates
+	Microsoft.VerifyProcedure = &VerifyProcedureMicrosoft{}
+}
+
+// InitializeApple sets up the built-in Apple Verifier.
+func InitializeApple(roots, intermediates *x509.CertPool) {
+	Apple.Roots = roots
+	Apple.Intermediates = intermediates
+	Apple.VerifyProcedure = &VerifyProcedureApple{}
+}
+
+// InitializeJava sets up the built-in Java Verifier.
+func InitializeJava(roots, intermediates *x509.CertPool) {
+	Java.Roots = roots
+	Java.Intermediates = intermediates
+	Java.VerifyProcedure = &VerifyProcedureJava{}
+}
