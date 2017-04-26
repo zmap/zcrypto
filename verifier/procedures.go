2a6822f314ce796feb747c069707cc5345c17a68
David Adrian <davidcadrian@gmail.com>
2017-04-26 11:41:45 -0400

    Reland 5a1d94c1219e5e7b12f6adecd4d47f102843a5a6 (#37)
    This doesn't actually touch any code that runs during ZGrab.
    
    ------
    
    Add initial zcrypto/verifier (#22)
    
    Verifier is a higher-level module for doing in-depth certificate
    verification, with the goal of being able to impersonate a browser or
    specific root store's validation procedure. It uses the chaining code in
    x509 to generate chains, and then apply's customizable verification on
    top.
    
    This includes a few "built-in" verifiers meant to represent the Firefox,
    Microsoft, Apple, and Java trust stores, respectively.
    
    The package is not yet complete, but should have roughly equivalent
    functionality to x509.ValidateWithStupidDetail, which is now deprecated.

---
+++ verifier/procedures.go
@@ -0,0 +1,30 @@
+/*
+ * ZCrypto Copyright 2017 Regents of the University of Michigan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package verifier
+
+// TODO: Implement these as the VerifyProcedure interface expands.
+
+// VerifyProcedureNSS implements the VerifyProcedure interface for NSS.
+type VerifyProcedureNSS struct{}
+
+// VerifyProcedureMicrosoft implements the VerifyProcedure interface for
+// Microsoft.
+type VerifyProcedureMicrosoft struct{}
+
+// VerifyProcedureApple implements the VerifyProcedure interface for Apple.
+type VerifyProcedureApple struct{}
+
+// VerifyProcedureJava implements the VerifyProcedure interface for Java.
+type VerifyProcedureJava struct{}
