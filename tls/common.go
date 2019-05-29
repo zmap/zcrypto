9d823b8edfedd338a26bbc6d7dd29a78b16bfe62
David Adrian <davidcadrian@gmail.com>
2019-05-29 17:48:31 -0400

    Update constants in common.go

--- tls/common.go
+++ tls/common.go
@@ -35,9 +35,11 @@ const (
 const (
 	maxPlaintext        = 16384        // maximum plaintext payload length
 	maxCiphertext       = 16384 + 2048 // maximum ciphertext payload length
-	tlsRecordHeaderLen  = 5            // record header length
+	maxCiphertextTLS13  = 16384 + 256  // maximum ciphertext length in TLS 1.3
+	recordHeaderLen     = 5            // record header length
 	dtlsRecordHeaderLen = 13
 	maxHandshake        = 65536 // maximum handshake we support (protocol max is 16 MB)
+	maxUselessRecords   = 16    // maximum number of consecutive non-advancing records
 
 	minVersion = VersionSSL30
 	maxVersion = VersionTLS12
