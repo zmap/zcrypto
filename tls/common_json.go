46a55613bf3c98840f7ca8a651571beebb5f8fea
David Adrian <davidcadrian@gmail.com>
2019-06-02 16:44:14 -0400

    Add common_json.go to TLS
    This file contains code added in by ZCrypto to handle JSON serializion
    of objects defined in common.go. This should allow for easier
    merges from upstream.

---
+++ tls/common_json.go
@@ -0,0 +1,46 @@
+package tls
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+// PointFormat is for TLS 1.2, and stores TLS Elliptic Curve Point Formats.
+type PointFormat uint8
+
+// MarshalJSON implements the json.Marshaler interface.
+func (pFormat *PointFormat) MarshalJSON() ([]byte, error) {
+	buf := make([]byte, 1)
+	buf[0] = byte(*pFormat)
+	enc := strings.ToUpper(hex.EncodeToString(buf))
+	aux := struct {
+		Hex   string `json:"hex"`
+		Name  string `json:"name"`
+		Value uint8  `json:"value"`
+	}{
+		Hex:   fmt.Sprintf("0x%s", enc),
+		Name:  pFormat.String(),
+		Value: uint8(*pFormat),
+	}
+
+	return json.Marshal(aux)
+}
+
+// UnmarshalJSON implements the json.Unmarshaler interface.
+func (pFormat *PointFormat) UnmarshalJSON(b []byte) error {
+	aux := struct {
+		Hex   string `json:"hex"`
+		Name  string `json:"name"`
+		Value uint8  `json:"value"`
+	}{}
+	if err := json.Unmarshal(b, &aux); err != nil {
+		return err
+	}
+	if expectedName := nameForPointFormat(aux.Value); expectedName != aux.Name {
+		return fmt.Errorf("mismatched point format and name, point format: %d, name: %s, expected name: %s", aux.Value, aux.Name, expectedName)
+	}
+	*pFormat = PointFormat(aux.Value)
+	return nil
+}
