zmap/zcrypto

commit 13cf40b46ed3697eba66054aca90c73bbbc991d4

Parse QC Statements (#217)
https://github.com/zmap/zcrypto/pull/217

zcrypto_schemas/zcrypto.py
@@ -39,6 +39,7 @@ DistinguishedName = SubRecordType({
     "jurisdiction_country":ListOf(WhitespaceAnalyzedString(), doc="jurisdictionCountry elements of the distinguished name (OBJECT IDENTIFIER 1.3.6.1.4.1.311.60.2.1.3)"),
     "jurisdiction_locality":ListOf(WhitespaceAnalyzedString(), doc="jurisdictionLocality elements of the distinguished name (OBJECT IDENTIFIER 1.3.6.1.4.1.311.60.2.1.1)"),
     "jurisdiction_province":ListOf(WhitespaceAnalyzedString(), doc="jurisdictionStateOrProvice elements of the distinguished name (OBJECT IDENTIFIER 1.3.6.1.4.1.311.60.2.1.2)"),
+    "organization_id": ListOf(WhitespaceAnalyzedString(), doc="organizationId elements of the distinguished name (OBJECT IDENTIFIER 2.5.4.97)"),
 })
 
 # x509/pkix/pkix.go: Extension (via auxExtension in x509/json.go)
@@ -59,6 +60,41 @@ EDIPartyName = SubRecordType({
 OtherName = SubRecordType({
     "id": OID(doc="The OBJECT IDENTIFIER identifying the syntax of the otherName value."),
     "value": IndexedBinary(doc="The raw otherName value."),
+})
+
+CABFOrganizationID = SubRecordType({
+    "scheme": WhitespaceAnalyzedString(),
+    "country": WhitespaceAnalyzedString(),
+    "state": WhitespaceAnalyzedString(),
+    "reference": WhitespaceAnalyzedString(),
+})
+
+
+QCStatementsExtensions = SubRecordType({
+    "ids": ListOf(OID(doc="All included statement OIDs")),
+    "parsed": SubRecordType({
+        "etsi_compliance": ListOf(Boolean(doc="True if present (Statement ID 0.4.0.1862.1.1)")),
+        "sscd": ListOf(Boolean(doc="True if present (Statement ID 0.4.0.1862.1.4")),
+        "types": ListOf(SubRecordType({
+            "ids": ListOf(OID(doc="Included QC type OIDs")),
+        }, doc="Statement ID 0.4.0.1862.1.6")),
+        "limit": ListOf(SubRecordType({
+            "currency": String(doc="Currency, if provided as a string"),
+            "currency_number": Signed64BitInteger(doc="Currency, if provided as an integer"),
+            "amount": Signed64BitInteger(doc="Value in currency"),
+            "exponent": Signed64BitInteger(doc="Total is amount times 10 raised to the exponent"),
+        }, doc="Statement ID 0.4.0.1862.1.2")),
+        "pds_locations": ListOf(SubRecordType({
+                "locations": ListOf(SubRecordType({
+                    "url": URL(doc="Location of the PDS"),
+                    "language": String(doc="Locale code"),
+            }, doc="PDS Location entry")),
+        }, doc="Statement ID 0.4.0.1862.1.5")),
+        "retention_period": ListOf(Signed64BitInteger(doc="Value of Statement ID 0.4.0.1862.1.3")),
+        "legislation": ListOf(SubRecordType({
+            "country_codes": ListOf(String(doc="Country codes for the set of countries where this certificate issued as a qualified certificate"))
+        }, doc="Value of Statement ID 0.4.0.1862.1.7")),
+    }, doc="Contains known QCStatements. Each field is repeated to handle the case where a single statement appears more than once."),
 })
 
 # x509/extensions.go: GeneralNames/jsonGeneralNames [RFC 5280 section 4.2.1.6]
@@ -389,7 +425,9 @@ ParsedCertificate = SubRecordType({
             "excluded_edi_party_names": ListOf(EDIPartyName(), doc="Excluded names of type ediPartyName."),
         }, category="Name Constraints", doc="The parsed id-ce-nameConstraints extension (2.5.29.30). Specifies a name space within which all child certificates' subject names MUST be located."),
         "signed_certificate_timestamps": ListOf(SCTRecord(), category="Embedded SCTS / CT Poison", doc="The parsed Certificate Transparency SignedCertificateTimestampsList extension (1.3.6.1.4.1.11129.2.4.2); see RFC 6962."),
-        "ct_poison": Boolean(category="Embedded SCTS / CT Poison", doc="This is true if the certificate possesses the Certificate Transparency Precertificate Poison extension (1.3.6.1.4.1.11129.2.4.3).")
+        "ct_poison": Boolean(category="Embedded SCTS / CT Poison", doc="This is true if the certificate possesses the Certificate Transparency Precertificate Poison extension (1.3.6.1.4.1.11129.2.4.3)."),
+        "cabf_organization_id": CABFOrganizationID(category="CABF Organization ID Extension", doc="The CA/BF organization ID extensions (2.23.140.3.1)"),
+        "qc_statements": QCStatementsExtensions(category="QC Statements Extension", doc="IDs and parsed statements for qualified certificates (1.3.6.1.5.5.7.1.3)"),
     }),
     "unknown_extensions": ListOf(UnknownExtension(), category="Unknown Extensions", doc="List of raw extensions that were not recognized by the application."),
     "signature": SubRecord({

x509/extensions.go
@@ -33,6 +33,7 @@ var (
 	oidExtSignedCertificateTimestampList = oidExtensionSignedCertificateTimestampList
 
 	oidExtCABFOrganizationID = asn1.ObjectIdentifier{2, 23, 140, 3, 1}
+	oidExtQCStatements       = asn1.ObjectIdentifier{1, 3, 6, 1, 5, 5, 7, 1, 3}
 )
 
 type CertificateExtensions struct {
@@ -51,6 +52,7 @@ type CertificateExtensions struct {
 	SignedCertificateTimestampList []*ct.SignedCertificateTimestamp `json:"signed_certificate_timestamps,omitempty"`
 	TorServiceDescriptors          []*TorServiceDescriptorHash      `json:"tor_service_descriptors,omitempty"`
 	CABFOrganizationIdentifier     *CABFOrganizationIdentifier      `json:"cabf_organization_id,omitempty"`
+	QCStatements                   *QCStatements                    `json:"qc_statements,omitempty"`
 }
 
 type UnknownCertificateExtensions []pkix.Extension
@@ -787,6 +789,8 @@ func (c *Certificate) jsonifyExtensions() (*CertificateExtensions, UnknownCertif
 			exts.TorServiceDescriptors = c.TorServiceDescriptors
 		} else if e.Id.Equal(oidExtCABFOrganizationID) {
 			exts.CABFOrganizationIdentifier = c.CABFOrganizationIdentifier
+		} else if e.Id.Equal(oidExtQCStatements) {
+			exts.QCStatements = c.QCStatements
 		} else {
 			// Unknown extension
 			unk = append(unk, e)

x509/json_test.go
@@ -53,7 +53,11 @@ func TestCertificateJSON(t *testing.T) {
 		},
 		{
 			file:     "qwac.pem",
-			expected: `{"version":3,"serial_number":"202093337738244911112370","signature_algorithm":{"name":"SHA256-RSA","oid":"1.2.840.113549.1.1.11"},"issuer":{"common_name":["Buypass Class 3 CA 2"],"country":["NO"],"organization":["Buypass AS-983163327"]},"issuer_dn":"C=NO, O=Buypass AS-983163327, CN=Buypass Class 3 CA 2","validity":{"start":"2020-02-04T14:05:09Z","end":"2022-02-03T22:59:00Z","length":63104031},"subject":{"common_name":["qwac.prod.vipps.no"],"serial_number":["918713867"],"country":["NO"],"locality":["OSLO"],"organization":["VIPPS AS"],"postal_code":["0150"],"jurisdiction_country":["NO"],"organization_id":["PSDNO-FSA-918713867"]},"subject_dn":"postalCode=0150, L=OSLO, businessCategory=Private Organization, jurisdictionCountry=NO, C=NO, O=VIPPS AS, CN=qwac.prod.vipps.no, organizationIdentifier=PSDNO-FSA-918713867, serialNumber=918713867","subject_key_info":{"key_algorithm":{"name":"RSA"},"rsa_public_key":{"exponent":65537,"modulus":"pZhG4M3ej2sh+PdDZdMP6mW2w0Ulw11O6xg2NWaU4qypfggxS+HC5QM65GGyvHZh0BlVTYs1zuIQSxeylitwcoyOLpv1kZtXtvhXjGlnJJJCXOJh6g86WeWrhUxHFOQQtvtFg7ZhaYpeyldabnHcDcyxq3LVQmRL6WQwSQgEHCIlXCSI5+DCcBKr9iZYokq0kAg6jCFJojhUypv/rRYS2C3HBWtlWiw1Ln0BeJVEzXaCyQsmtX/TQb0W4O1YhGmvj7fC+P+mQh4PJqwd2mU5CGjcJWLdd664TudjU8uDiR7/VgwaCbi0m8ugcJNxvZ3/yLElj95aHY6gKG+rmciIGQ==","length":2048},"fingerprint_sha256":"7e0b4098d838ebe5238997c5418ea20b96a1f7e9b643293885197d609d09e77c"},"extensions":{"key_usage":{"digital_signature":true,"key_encipherment":true,"value":5},"basic_constraints":{"is_ca":false},"subject_alt_name":{"dns_names":["qwac.prod.vipps.no"]},"crl_distribution_points":["http://crl.buypass.no/crl/BPClass3CA2.crl"],"authority_key_id":"22302ed2fbf64bcac0b83bd204c4e972e6979b0c","subject_key_id":"a002f1a7fd4a9e9932efeebaa1a3aa1e2ee7eec5","extended_key_usage":{"server_auth":true,"client_auth":true},"certificate_policies":[{"id":"2.16.578.1.26.1.3.3","cps":["https://www.buypass.no/cps"]},{"id":"2.23.140.1.1"},{"id":"0.4.0.194112.1.4"}],"authority_info_access":{"ocsp_urls":["http://ocsp.buypass.com"],"issuer_urls":["http://crt.buypass.no/crt/BPClass3CA2.cer"]},"ct_poison":true,"cabf_organization_id":{"scheme":"PSD","country":"NO","reference":"918713867"}},"unknown_extensions":[{"id":"1.3.6.1.5.5.7.1.3","critical":false,"value":"MIHDMAgGBgQAjkYBATBpBgYEAIGYJwIwXzAmMBEGBwQAgZgnAQMMBlBTUF9BSTARBgcEAIGYJwECDAZQU1BfUEkMLVRoZSBGaW5hbmNpYWwgU3VwZXJ2aXNvcnkgQXV0aG9yaXR5IG9mIE5vcndheQwGTk8tRlNBMBMGBgQAjkYBBjAJBgcEAI5GAQYDMDcGBgQAjkYBBTAtMCsWJWh0dHBzOi8vd3d3LmJ1eXBhc3Mubm8vcGRzL3Bkc19lbi5wZGYTAmVu"}],"signature":{"signature_algorithm":{"name":"SHA256-RSA","oid":"1.2.840.113549.1.1.11"},"value":"C6EnzHU7tGVa2HUw0Y3KaRtgSkF3FNAsO63VRac/SZMykM60bM9nuwdy46/o01iQ+wi+P/kS5r6UYIBOwLOmv6PrMuj+nV3YhADI9/A1R4MqEjKWHzVefTXUT6vWOm7JDf2n4H3wdh/LxdfjxEn01/NcCPh+HEgatldcoXpnirVKctO2Kgg5dD2uLWtPQixr2aJQx+4wALCBG7cwKmp0M9CLJv35+O2/1dMwhm95NfpcX6t53F7M9720fTeFTYALX1jdN2SxA7VyHqhkc0poTNtKtutRkMMWkwcDSz/FD0NMjJdsaVIJ+bdB7gh7JT1hd4CBJDKRhNRq9aSGGIcEeQ==","valid":false,"self_signed":false},"fingerprint_md5":"ed91f4dc129bb1e645e36454b4955fd6","fingerprint_sha1":"f1b7533029586920ee56df926bc59dcd8cfa3630","fingerprint_sha256":"f42775eeb4baa39ab4ba0e3a37ee2f30a41267274ea20a75ae2721cae39f9a83","tbs_noct_fingerprint":"cff7cb3009a22b725e4d3d71762c377cbdefd196cff2712a0893e2c29870b320","spki_subject_fingerprint":"b4e3dff7dbac7fa5744f916ddaac9bb7ac09f6914c523d8cd356b543b621802c","tbs_fingerprint":"e5d2338b23acb820d285a04b800aa4f9fc162c8c919cb2b957f2b2209848a0b2","validation_level":"EV","names":["qwac.prod.vipps.no"],"redacted":false}`,
+			expected: `{"version":3,"serial_number":"202093337738244911112370","signature_algorithm":{"name":"SHA256-RSA","oid":"1.2.840.113549.1.1.11"},"issuer":{"common_name":["Buypass Class 3 CA 2"],"country":["NO"],"organization":["Buypass AS-983163327"]},"issuer_dn":"C=NO, O=Buypass AS-983163327, CN=Buypass Class 3 CA 2","validity":{"start":"2020-02-04T14:05:09Z","end":"2022-02-03T22:59:00Z","length":63104031},"subject":{"common_name":["qwac.prod.vipps.no"],"serial_number":["918713867"],"country":["NO"],"locality":["OSLO"],"organization":["VIPPS AS"],"postal_code":["0150"],"jurisdiction_country":["NO"],"organization_id":["PSDNO-FSA-918713867"]},"subject_dn":"postalCode=0150, L=OSLO, businessCategory=Private Organization, jurisdictionCountry=NO, C=NO, O=VIPPS AS, CN=qwac.prod.vipps.no, organizationIdentifier=PSDNO-FSA-918713867, serialNumber=918713867","subject_key_info":{"key_algorithm":{"name":"RSA"},"rsa_public_key":{"exponent":65537,"modulus":"pZhG4M3ej2sh+PdDZdMP6mW2w0Ulw11O6xg2NWaU4qypfggxS+HC5QM65GGyvHZh0BlVTYs1zuIQSxeylitwcoyOLpv1kZtXtvhXjGlnJJJCXOJh6g86WeWrhUxHFOQQtvtFg7ZhaYpeyldabnHcDcyxq3LVQmRL6WQwSQgEHCIlXCSI5+DCcBKr9iZYokq0kAg6jCFJojhUypv/rRYS2C3HBWtlWiw1Ln0BeJVEzXaCyQsmtX/TQb0W4O1YhGmvj7fC+P+mQh4PJqwd2mU5CGjcJWLdd664TudjU8uDiR7/VgwaCbi0m8ugcJNxvZ3/yLElj95aHY6gKG+rmciIGQ==","length":2048},"fingerprint_sha256":"7e0b4098d838ebe5238997c5418ea20b96a1f7e9b643293885197d609d09e77c"},"extensions":{"key_usage":{"digital_signature":true,"key_encipherment":true,"value":5},"basic_constraints":{"is_ca":false},"subject_alt_name":{"dns_names":["qwac.prod.vipps.no"]},"crl_distribution_points":["http://crl.buypass.no/crl/BPClass3CA2.crl"],"authority_key_id":"22302ed2fbf64bcac0b83bd204c4e972e6979b0c","subject_key_id":"a002f1a7fd4a9e9932efeebaa1a3aa1e2ee7eec5","extended_key_usage":{"server_auth":true,"client_auth":true},"certificate_policies":[{"id":"2.16.578.1.26.1.3.3","cps":["https://www.buypass.no/cps"]},{"id":"2.23.140.1.1"},{"id":"0.4.0.194112.1.4"}],"authority_info_access":{"ocsp_urls":["http://ocsp.buypass.com"],"issuer_urls":["http://crt.buypass.no/crt/BPClass3CA2.cer"]},"ct_poison":true,"cabf_organization_id":{"scheme":"PSD","country":"NO","reference":"918713867"},"qc_statements":{"ids":["0.4.0.1862.1.1","0.4.0.19495.2","0.4.0.1862.1.6","0.4.0.1862.1.5"],"parsed":{"etsi_compliance":[true],"types":[{"ids":["0.4.0.1862.1.6.3"]}],"pds_locations":[{"locations":[{"url":"https://www.buypass.no/pds/pds_en.pdf","language":"en"}]}]}}},"signature":{"signature_algorithm":{"name":"SHA256-RSA","oid":"1.2.840.113549.1.1.11"},"value":"C6EnzHU7tGVa2HUw0Y3KaRtgSkF3FNAsO63VRac/SZMykM60bM9nuwdy46/o01iQ+wi+P/kS5r6UYIBOwLOmv6PrMuj+nV3YhADI9/A1R4MqEjKWHzVefTXUT6vWOm7JDf2n4H3wdh/LxdfjxEn01/NcCPh+HEgatldcoXpnirVKctO2Kgg5dD2uLWtPQixr2aJQx+4wALCBG7cwKmp0M9CLJv35+O2/1dMwhm95NfpcX6t53F7M9720fTeFTYALX1jdN2SxA7VyHqhkc0poTNtKtutRkMMWkwcDSz/FD0NMjJdsaVIJ+bdB7gh7JT1hd4CBJDKRhNRq9aSGGIcEeQ==","valid":false,"self_signed":false},"fingerprint_md5":"ed91f4dc129bb1e645e36454b4955fd6","fingerprint_sha1":"f1b7533029586920ee56df926bc59dcd8cfa3630","fingerprint_sha256":"f42775eeb4baa39ab4ba0e3a37ee2f30a41267274ea20a75ae2721cae39f9a83","tbs_noct_fingerprint":"cff7cb3009a22b725e4d3d71762c377cbdefd196cff2712a0893e2c29870b320","spki_subject_fingerprint":"b4e3dff7dbac7fa5744f916ddaac9bb7ac09f6914c523d8cd356b543b621802c","tbs_fingerprint":"e5d2338b23acb820d285a04b800aa4f9fc162c8c919cb2b957f2b2209848a0b2","validation_level":"EV","names":["qwac.prod.vipps.no"],"redacted":false}`,
+		},
+		{
+			file:     "etsi_qc.pem",
+			expected: `{"version":3,"serial_number":"9591198274709138036589902159","signature_algorithm":{"name":"SHA256-RSA","oid":"1.2.840.113549.1.1.11"},"issuer":{"common_name":["Development Sub CA"],"country":["PK"],"organization":["Development"]},"issuer_dn":"C=PK, O=Development, CN=Development Sub CA","validity":{"start":"2019-09-25T09:37:57Z","end":"2029-10-26T09:37:57Z","length":318297600},"subject":{"common_name":["Muhammad Bilal Ashraf"],"serial_number":["578611675"],"country":["PK"],"organization":["Development"],"given_name":["Muhammad Bilal"],"surname":["Ashraf"]},"subject_dn":"C=PK, O=Development, serialNumber=578611675, GN=Muhammad Bilal, SN=Ashraf, CN=Muhammad Bilal Ashraf","subject_key_info":{"key_algorithm":{"name":"RSA"},"rsa_public_key":{"exponent":65537,"modulus":"2HPVolNvA0qVuwXPcXIdra/W/VOLzx5RyRjAHUC8ssYKPLJ5rNjUcAFrIMRBELVw1B+Sr3/671cDsh12nFkO4KfCpprvG9gpV2yLZPNhpUPqxpbXa6H6VQv4PYNHuf75kPJzyn2dkhw/RGNfiN9b/qM4LSxHzlrOe+gjN7uSaLEctr16v7lu650lbNC29XfG9FuRKeHAB04WoFtge6P3XwvRkHTVvcUjx0XSRD3Fbcvi/SqKZStFLN382Xp/uA7S+kX0eVHHyIgQyEydOq5lI1mTRCsJn0vOXav2YgZiHGvpDMzXPz/j/iAFblLmGeNFRUQDIrwTYUEfdGjBGOAlVw==","length":2048},"fingerprint_sha256":"bc898e576ca5b03513505d6433e91b4ada07f86a6ce3412d088db9580b13500e"},"extensions":{"key_usage":{"digital_signature":true,"content_commitment":true,"value":3},"basic_constraints":{"is_ca":false},"subject_alt_name":{"email_addresses":["bilal.ashraf@gmail.com"]},"crl_distribution_points":["http://dev.com/ca.crl"],"authority_key_id":"30cd83a736aca535ff211b37406eb2cf5f1c032f","subject_key_id":"5e3b7f1de8d5587eee26b3c792e65ac10e5fbb9d","extended_key_usage":{"email_protection":true},"certificate_policies":[{"id":"1.3.7.8.9","cps":["https://www.dev.com/repository/"]}],"authority_info_access":{"ocsp_urls":["http://dev.com/ocsp"],"issuer_urls":["http://dev.com/ca.crt"]},"qc_statements":{"ids":["0.4.0.1862.1.1","0.4.0.1862.1.3","0.4.0.1862.1.2","0.4.0.1862.1.4","0.4.0.1862.1.5","0.4.0.1862.1.6"],"parsed":{"etsi_compliance":[true],"sscd":[true],"types":[{"ids":["0.4.0.1862.1.6.1","0.4.0.1862.1.6.2","0.4.0.1862.1.6.3"]}],"limit":[{"currency":"EURO","amount":10,"exponent":2}],"pds_locations":[{"locations":[{"url":"https://dev.com/pds/en/pds.pdf","language":"en"}]}],"retention_period":[10]}}},"signature":{"signature_algorithm":{"name":"SHA256-RSA","oid":"1.2.840.113549.1.1.11"},"value":"DyWYTIzvGbzIbUbfe0mHSNxjoacuhWWQLhyX/UfHZ5hdYL7osIvwvWu81hGiGK9Eo0279W0YexJ4qp90YIzG8kjKvK7XIbFLmYnCf7+8sXHcvH9w/b8W11cTWmC2+xuYgxCWvHkGwWVpEpZlkDc+YYjilUVQ7wIcingpT98Tek36RIZ8mcNqkXUTrCaWt9Ra4h732J87MSWydifEMYUi+fSKttw7cjBzf6Q5XFkWY0uQtY087EIRldezwa9CZASStaEVzQ7UTsOzLSbmYEQzKK8siO6Z+RjV86ZU47uSZ2cC++/SQBufAQ0Ch/tnN2O3vACfA30QhQblWHe9Wd4taA==","valid":false,"self_signed":false},"fingerprint_md5":"173780910e4430a16d0e7a4ed6667b9f","fingerprint_sha1":"ce69f32ca243df47f6fccb985d6d340b15d5a827","fingerprint_sha256":"a5885ab12e6260345007884baa9f1ca7343719d80381e3a7653be314e80dea34","tbs_noct_fingerprint":"5753c966f66d7db686320109cc4f79553fa25aa45917b8326aab7e9a42507f3b","spki_subject_fingerprint":"af9c1632cdf9bc7b1041ca90006c6956b44cfc0b224ff0c19228b980e8e1b280","tbs_fingerprint":"5753c966f66d7db686320109cc4f79553fa25aa45917b8326aab7e9a42507f3b","validation_level":"unknown","redacted":false}`,
 		},
 	}
 	for i, test := range tests {

x509/qc_statements.go
@@ -1,0 +1,166 @@
+package x509
+
+import (
+	"encoding/asn1"
+	"encoding/json"
+)
+
+type QCStatementASN struct {
+	StatementID   asn1.ObjectIdentifier
+	StatementInfo asn1.RawValue `asn1:"optional"`
+}
+
+func (s *QCStatementASN) MarshalJSON() ([]byte, error) {
+	aux := struct {
+		ID    string `json:"id,omitempty"`
+		Value []byte `json:"value,omitempty"`
+	}{
+		ID:    s.StatementID.String(),
+		Value: s.StatementInfo.Bytes,
+	}
+	return json.Marshal(&aux)
+}
+
+type QCStatementsASN struct {
+	QCStatements []QCStatementASN
+}
+
+// ETSI OIDS from https://www.etsi.org/deliver/etsi_en/319400_319499/31941205/02.02.03_20/en_31941205v020203a.pdf
+var (
+	oidEtsiQcsQcCompliance      = asn1.ObjectIdentifier{0, 4, 0, 1862, 1, 1}
+	oidEtsiQcsQcLimitValue      = asn1.ObjectIdentifier{0, 4, 0, 1862, 1, 2}
+	oidEtsiQcsQcRetentionPeriod = asn1.ObjectIdentifier{0, 4, 0, 1862, 1, 3}
+	oidEtsiQcsQcSSCD            = asn1.ObjectIdentifier{0, 4, 0, 1862, 1, 4}
+	oidEtsiQcsQcEuPDS           = asn1.ObjectIdentifier{0, 4, 0, 1862, 1, 5}
+	oidEtsiQcsQcType            = asn1.ObjectIdentifier{0, 4, 0, 1862, 1, 6}
+	oidEtsiQcsQcCCLegislation   = asn1.ObjectIdentifier{0, 4, 0, 1862, 1, 7}
+	oidEtsiQcsQctEsign          = asn1.ObjectIdentifier{0, 4, 0, 1862, 1, 6, 1}
+	oidEtsiQcsQctEseal          = asn1.ObjectIdentifier{0, 4, 0, 1862, 1, 6, 2}
+	oidEtsiQcsQctWeb            = asn1.ObjectIdentifier{0, 4, 0, 1862, 1, 6, 3}
+)
+
+type QCStatements struct {
+	StatementIDs     []string            `json:"ids,omitempty"`
+	ParsedStatements *ParsedQCStatements `json:"parsed,omitempty"`
+}
+
+type ParsedQCStatements struct {
+	ETSICompliance  []bool          `json:"etsi_compliance,omitempty"`
+	SSCD            []bool          `json:"sscd,omitempty"`
+	Types           []QCType        `json:"types,omitempty"`
+	Limit           []MonetaryValue `json:"limit,omitempty"`
+	PDSLocations    []PDSLocations  `json:"pds_locations,omitempty"`
+	RetentionPeriod []int           `json:"retention_period,omitempty"`
+	Legislation     []QCLegistation `json:"legislation,omitempty"`
+}
+
+type MonetaryValue struct {
+	Currency       string `json:"currency,omitempty"`
+	CurrencyNumber int    `json:"currency_number,omitempty"`
+	Amount         int    `json:"amount,omitempty"`
+	Exponent       int    `json:"exponent,omitempty"`
+}
+
+type monetaryValueASNString struct {
+	Currency string `asn1:"printable"`
+	Amount   int
+	Exponent int
+}
+
+type monetaryValueASNNumber struct {
+	Currency int
+	Amount   int
+	Exponent int
+}
+
+type PDSLocations struct {
+	Locations []PDSLocation `json:"locations,omitempty"`
+}
+
+type PDSLocation struct {
+	URL      string `json:"url,omitempty" asn1:"ia5"`
+	Language string `json:"language,omitempty" asn1:"printable"`
+}
+
+type QCType struct {
+	TypeIdentifiers []asn1.ObjectIdentifier
+}
+
+type QCLegistation struct {
+	CountryCodes []string `json:"country_codes,omitempty"`
+}
+
+func (qt *QCType) MarshalJSON() ([]byte, error) {
+	aux := struct {
+		Types []string `json:"ids,omitempty"`
+	}{
+		Types: make([]string, len(qt.TypeIdentifiers)),
+	}
+	for idx := range qt.TypeIdentifiers {
+		aux.Types[idx] = qt.TypeIdentifiers[idx].String()
+	}
+	return json.Marshal(&aux)
+}
+
+func (q *QCStatements) Parse(in *QCStatementsASN) error {
+	q.StatementIDs = make([]string, len(in.QCStatements))
+	known := ParsedQCStatements{}
+	for i, s := range in.QCStatements {
+		val := in.QCStatements[i].StatementInfo.FullBytes
+		q.StatementIDs[i] = s.StatementID.String()
+		if s.StatementID.Equal(oidEtsiQcsQcCompliance) {
+			known.ETSICompliance = append(known.ETSICompliance, true)
+		} else if s.StatementID.Equal(oidEtsiQcsQcLimitValue) {
+			// TODO
+			mvs := monetaryValueASNString{}
+			mvn := monetaryValueASNNumber{}
+			out := MonetaryValue{}
+			if _, err := asn1.Unmarshal(val, &mvs); err == nil {
+				out.Currency = mvs.Currency
+				out.Amount = mvs.Amount
+				out.Exponent = mvs.Exponent
+			} else if _, err := asn1.Unmarshal(val, &mvn); err == nil {
+				out.CurrencyNumber = mvn.Currency
+				out.Amount = mvn.Amount
+				out.Exponent = mvn.Exponent
+			} else {
+				return err
+			}
+			known.Limit = append(known.Limit, out)
+		} else if s.StatementID.Equal(oidEtsiQcsQcRetentionPeriod) {
+			var retentionPeriod int
+			if _, err := asn1.Unmarshal(val, &retentionPeriod); err != nil {
+				return err
+			}
+			known.RetentionPeriod = append(known.RetentionPeriod, retentionPeriod)
+		} else if s.StatementID.Equal(oidEtsiQcsQcSSCD) {
+			known.SSCD = append(known.SSCD, true)
+		} else if s.StatementID.Equal(oidEtsiQcsQcEuPDS) {
+			locations := make([]PDSLocation, 0)
+			if _, err := asn1.Unmarshal(val, &locations); err != nil {
+				return err
+			}
+			known.PDSLocations = append(known.PDSLocations, PDSLocations{
+				Locations: locations,
+			})
+		} else if s.StatementID.Equal(oidEtsiQcsQcType) {
+			typeIds := make([]asn1.ObjectIdentifier, 0)
+			if _, err := asn1.Unmarshal(val, &typeIds); err != nil {
+				return err
+			}
+			known.Types = append(known.Types, QCType{
+				TypeIdentifiers: typeIds,
+			})
+		} else if s.StatementID.Equal(oidEtsiQcsQcCCLegislation) {
+			countryCodes := make([]string, 0)
+			if _, err := asn1.Unmarshal(val, &countryCodes); err != nil {
+				return err
+			}
+			known.Legislation = append(known.Legislation, QCLegistation{
+				CountryCodes: countryCodes,
+			})
+		}
+	}
+	q.ParsedStatements = &known
+	return nil
+}

x509/testdata/etsi_qc.pem
@@ -1,0 +1,31 @@
+-----BEGIN CERTIFICATE-----
+MIIFRzCCBC+gAwIBAgIMHv2nPE8W61evcLlPMA0GCSqGSIb3DQEBCwUAMEAxCzAJ
+BgNVBAYTAlBLMRQwEgYDVQQKEwtEZXZlbG9wbWVudDEbMBkGA1UEAxMSRGV2ZWxv
+cG1lbnQgU3ViIENBMB4XDTE5MDkyNTA5Mzc1N1oXDTI5MTAyNjA5Mzc1N1owgYEx
+CzAJBgNVBAYTAlBLMRQwEgYDVQQKEwtEZXZlbG9wbWVudDESMBAGA1UEBRMJNTc4
+NjExNjc1MRcwFQYDVQQqEw5NdWhhbW1hZCBCaWxhbDEPMA0GA1UEBBMGQXNocmFm
+MR4wHAYDVQQDExVNdWhhbW1hZCBCaWxhbCBBc2hyYWYwggEiMA0GCSqGSIb3DQEB
+AQUAA4IBDwAwggEKAoIBAQDYc9WiU28DSpW7Bc9xch2tr9b9U4vPHlHJGMAdQLyy
+xgo8snms2NRwAWsgxEEQtXDUH5Kvf/rvVwOyHXacWQ7gp8Kmmu8b2ClXbItk82Gl
+Q+rGltdrofpVC/g9g0e5/vmQ8nPKfZ2SHD9EY1+I31v+ozgtLEfOWs576CM3u5Jo
+sRy2vXq/uW7rnSVs0Lb1d8b0W5Ep4cAHThagW2B7o/dfC9GQdNW9xSPHRdJEPcVt
+y+L9KoplK0Us3fzZen+4DtL6RfR5UcfIiBDITJ06rmUjWZNEKwmfS85dq/ZiBmIc
+a+kMzNc/P+P+IAVuUuYZ40VFRAMivBNhQR90aMEY4CVXAgMBAAGjggH9MIIB+TAO
+BgNVHQ8BAf8EBAMCBsAwEwYDVR0lBAwwCgYIKwYBBQUHAwQwDAYDVR0TAQH/BAIw
+ADAdBgNVHQ4EFgQUXjt/HejVWH7uJrPHkuZawQ5fu50wHwYDVR0jBBgwFoAUMM2D
+pzaspTX/IRs3QG6yz18cAy8wUgYIKwYBBQUHAQEERjBEMB8GCCsGAQUFBzABhhNo
+dHRwOi8vZGV2LmNvbS9vY3NwMCEGCCsGAQUFBzAChhVodHRwOi8vZGV2LmNvbS9j
+YS5jcnQwIQYDVR0RBBowGIEWYmlsYWwuYXNocmFmQGdtYWlsLmNvbTAmBgNVHR8E
+HzAdMBugGaAXhhVodHRwOi8vZGV2LmNvbS9jYS5jcmwwQAYDVR0gBDkwNzA1BgQr
+BwgJMC0wKwYIKwYBBQUHAgEWH2h0dHBzOi8vd3d3LmRldi5jb20vcmVwb3NpdG9y
+eS8wgaIGCCsGAQUFBwEDBIGVMIGSMAgGBgQAjkYBATALBgYEAI5GAQMCAQowFgYG
+BACORgECMAwTBEVVUk8CAQoCAQIwCAYGBACORgEEMDAGBgQAjkYBBTAmMCQWHmh0
+dHBzOi8vZGV2LmNvbS9wZHMvZW4vcGRzLnBkZhMCZW4wJQYGBACORgEGMBsGBwQA
+jkYBBgEGBwQAjkYBBgIGBwQAjkYBBgMwDQYJKoZIhvcNAQELBQADggEBAA8lmEyM
+7xm8yG1G33tJh0jcY6GnLoVlkC4cl/1Hx2eYXWC+6LCL8L1rvNYRohivRKNNu/Vt
+GHsSeKqfdGCMxvJIyryu1yGxS5mJwn+/vLFx3Lx/cP2/FtdXE1pgtvsbmIMQlrx5
+BsFlaRKWZZA3PmGI4pVFUO8CHIp4KU/fE3pN+kSGfJnDapF1E6wmlrfUWuIe99if
+OzElsnYnxDGFIvn0irbcO3Iwc3+kOVxZFmNLkLWNPOxCEZXXs8GvQmQEkrWhFc0O
+1E7Dsy0m5mBEMyivLIjumfkY1fOmVOO7kmdnAvvv0kAbnwENAof7Zzdjt7wAnwN9
+EIUG5Vh3vVneLWg=
+-----END CERTIFICATE-----

x509/x509.go
@@ -823,6 +823,7 @@ type Certificate struct {
 
 	// QWACS
 	CABFOrganizationIdentifier *CABFOrganizationIdentifier
+	QCStatements               *QCStatements
 
 	// Used to speed up the zlint checks. Populated by the GetParsedDNSNames method.
 	parsedDNSNames []ParsedDomainName
@@ -1940,6 +1941,17 @@ func parseCertificate(in *certificate) (*Certificate, error) {
 				Reference: cabf.RegistrationReference,
 				State:     cabf.RegistrationStateOrProvince,
 			}
+		} else if e.Id.Equal(oidExtQCStatements) {
+			rawStatements := QCStatementsASN{}
+			_, err := asn1.Unmarshal(e.Value, &rawStatements.QCStatements)
+			if err != nil {
+				return nil, err
+			}
+			qcStatements := QCStatements{}
+			if err := qcStatements.Parse(&rawStatements); err != nil {
+				return nil, err
+			}
+			out.QCStatements = &qcStatements
 		}
 
 		//if e.Critical {

commit 0b8e6e5fef4f57be621e2880c9f707f97d611c7f

Fixup syntax issues in schema
Fixes regessions caused in 13cf40b46ed3697eba66054aca90c73bbbc991d4

zcrypto_schemas/zcrypto.py
@@ -69,31 +69,40 @@ CABFOrganizationID = SubRecordType({
     "reference": WhitespaceAnalyzedString(),
 })
 
+QCTypes = SubRecordType({
+    "ids": ListOf(OID(doc="Included QC type OIDs")),
+})
+
+MonetaryLimit = SubRecordType({
+    "currency": String(doc="Currency, if provided as a string"),
+    "currency_number": Signed64BitInteger(doc="Currency, if provided as an integer"),
+    "amount": Signed64BitInteger(doc="Value in currency"),
+    "exponent": Signed64BitInteger(doc="Total is amount times 10 raised to the exponent"),
+}, doc="Value limit for a financial transaction")
+
+PDSLocation = SubRecordType({
+    "url": URL(doc="Location of the PDS"),
+    "language": String(doc="Locale code"),
+}, doc="PDS Location entry")
+
+PDSLocations = SubRecordType({
+    "locations": ListOf(PDSLocation(), doc="Included PDS locations"),
+})
+
+QCLegislation = SubRecordType({
+    "country_codes": ListOf(String(doc="Country codes for the set of countries where this certificate issued as a qualified certificate"))
+}, doc="List of countries where this certificate is qualified")
 
 QCStatementsExtensions = SubRecordType({
     "ids": ListOf(OID(doc="All included statement OIDs")),
-    "parsed": SubRecordType({
+    "parsed": SubRecord({
         "etsi_compliance": ListOf(Boolean(doc="True if present (Statement ID 0.4.0.1862.1.1)")),
         "sscd": ListOf(Boolean(doc="True if present (Statement ID 0.4.0.1862.1.4")),
-        "types": ListOf(SubRecordType({
-            "ids": ListOf(OID(doc="Included QC type OIDs")),
-        }, doc="Statement ID 0.4.0.1862.1.6")),
-        "limit": ListOf(SubRecordType({
-            "currency": String(doc="Currency, if provided as a string"),
-            "currency_number": Signed64BitInteger(doc="Currency, if provided as an integer"),
-            "amount": Signed64BitInteger(doc="Value in currency"),
-            "exponent": Signed64BitInteger(doc="Total is amount times 10 raised to the exponent"),
-        }, doc="Statement ID 0.4.0.1862.1.2")),
-        "pds_locations": ListOf(SubRecordType({
-                "locations": ListOf(SubRecordType({
-                    "url": URL(doc="Location of the PDS"),
-                    "language": String(doc="Locale code"),
-            }, doc="PDS Location entry")),
-        }, doc="Statement ID 0.4.0.1862.1.5")),
-        "retention_period": ListOf(Signed64BitInteger(doc="Value of Statement ID 0.4.0.1862.1.3")),
-        "legislation": ListOf(SubRecordType({
-            "country_codes": ListOf(String(doc="Country codes for the set of countries where this certificate issued as a qualified certificate"))
-        }, doc="Value of Statement ID 0.4.0.1862.1.7")),
+        "types": ListOf(QCTypes(), doc="Statement ID 0.4.0.1862.1.6"),
+        "limit": ListOf(MonetaryLimit(), doc="Statement ID 0.4.0.1862.1.2"),
+        "pds_locations": ListOf(PDSLocations(), doc="Statement ID 0.4.0.1862.1.5"),
+        "retention_period": ListOf(Signed64BitInteger(), doc="Statement ID 0.4.0.1862.1.3"),
+        "legislation": ListOf(QCLegislation(), doc="Statement ID 0.4.0.1862.1.7"),
     }, doc="Contains known QCStatements. Each field is repeated to handle the case where a single statement appears more than once."),
 })
 